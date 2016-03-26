78facfa7cd3f7b4d6b60f2318bc42973657df649
heyx3 <manning.w@husky.neu.edu>
2016-03-26 03:03:53 -0400

    Add polish, metaball for ship

---
+++ VRRacer/Source/VRRacer/Private/ShipMetaball.cpp
@@ -0,0 +1,270 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "VRRacer.h"
+#include "ShipMetaball.h"
+
+#include <string>
+
+
+namespace
+{
+    const int32 N_BALLS = 5;
+}
+
+
+// Sets default values
+AShipMetaball::AShipMetaball()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+
+    //Default values.
+
+    localBallForce = FVector::ZeroVector;
+
+    BallPushback = 2.0f;
+    BallPushbackRadiusScale = 0.5f;
+
+    MinEmissive = 0.5f;
+    MaxEmissive = 20.0f;
+    Thinness = 100.0f;
+    Increment = 8.0f;
+    RadiusScale = 1.0f;
+    CurrentEmissive = 0.5f;
+    VoidColor = FColor::White;
+    BallColor = FColor::White;
+
+    UpdateVels = 0;
+
+    static ConstructorHelpers::FObjectFinder<UTexture2D> voidTex(TEXT("/Game/StarterContent/TExtures/T_Concrete_Grime_D.T_Concrete_Grime_D"));
+    if (voidTex.Succeeded())
+    {
+        VoidTex = voidTex.Object;
+    }
+
+    VoidTexScale = 1.0f;
+
+
+    //Set up components.
+
+    USceneComponent* rootSceneComp = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+    RootComponent = rootSceneComp;
+
+    MyRenderBox = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Metaball Box"));
+    MyGlassContainer = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Glass Container"));
+
+    MyRenderBox->AttachTo(RootComponent);
+    MyGlassContainer->AttachTo(MyRenderBox);
+
+    //Load the box mesh for both components.
+    static ConstructorHelpers::FObjectFinder<UStaticMesh> unitBoxMesh(TEXT("/Game/My_Content/Meshes/UnitBox.UnitBox"));
+    if (unitBoxMesh.Succeeded())
+    {
+        MyRenderBox->SetStaticMesh(unitBoxMesh.Object);
+        MyRenderBox->SetRelativeLocation(FVector(0.0f));
+        MyRenderBox->SetWorldScale3D(FVector(1.0f));
+        MyRenderBox->bGenerateOverlapEvents = 0;
+        MyRenderBox->SetSimulatePhysics(false);
+        MyRenderBox->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+        
+
+        MyGlassContainer->SetStaticMesh(unitBoxMesh.Object);
+        MyGlassContainer->SetRelativeLocation(FVector(0.0f));
+        MyGlassContainer->SetWorldScale3D(FVector(1.1f));
+        MyGlassContainer->bGenerateOverlapEvents = 0;
+        MyGlassContainer->SetSimulatePhysics(false);
+        MyGlassContainer->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+        static ConstructorHelpers::FObjectFinder<UMaterial> glassMat(TEXT("/Game/My_Content/Materials/MetaballContainerMat.MetaballContainerMat"));
+        if (glassMat.Succeeded())
+        {
+            MyGlassContainer->SetMaterial(0, glassMat.Object);
+        }
+    }
+
+
+    //Generate starting data for the balls.
+    BallPoses.Empty();
+    BallRadii.Empty();
+    BallVelocities.Empty();
+    for (int i = 0; i < N_BALLS; ++i)
+    {
+        BallPoses.Add(FMath::RandPointInBox(FBox(FVector(-1.0f), FVector(1.0f))));
+        BallVelocities.Add(FMath::VRand());
+        BallRadii.Add(0.25f);
+    }
+
+
+    //Create a dynamic version of the metaball material.
+    static ConstructorHelpers::FObjectFinder<UMaterial> mat(TEXT("Material'/Game/My_Content/Materials/MetaballMat.MetaballMat'"));
+    if (mat.Succeeded())
+    {
+        MetaballMat = MyRenderBox->CreateAndSetMaterialInstanceDynamicFromMaterial(0, mat.Object);
+        UpdateRenderingStuff(false);
+        MyRenderBox->SetMaterial(0, MetaballMat);
+    }
+    else
+    {
+        MetaballMat = nullptr;
+    }
+}
+void AShipMetaball::PostInitializeComponents()
+{
+    Super::PostInitializeComponents();
+}
+
+// Called when the game starts or when spawned
+void AShipMetaball::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AShipMetaball::AddBallForce(FVector worldForce)
+{
+    localBallForce += GetTransform().InverseTransformVector(worldForce);
+}
+void AShipMetaball::ResetVelocities()
+{
+    for (int i = 0; i < N_BALLS; ++i)
+        BallVelocities[i] = FMath::VRand();
+}
+
+// Called every frame
+void AShipMetaball::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+    if (UpdateVels != 0)
+    {
+        UpdateVels = 0;
+        ResetVelocities();
+    }
+
+    UpdateRenderingStuff(true);
+
+
+    //Update ball physics.
+    FTransform tr = GetTransform();
+    for (int32 i = 0; i < N_BALLS; ++i)
+    {
+        FVector& ballVel = BallVelocities[i];
+        FVector& ballPos = BallPoses[i];
+        float& ballRad = BallRadii[i];
+        float scaledRad = BallPushbackRadiusScale * ballRad;
+        float invRad = 1.0f / scaledRad;
+
+        //Apply force to the ball.
+        ballVel += localBallForce * DeltaTime;
+
+        //If it's being crushed against the edge of the bounds, push back.
+        float p;
+        float temp = BallPushback * invRad;
+        p = ballPos.X - scaledRad;
+        if (p < -1.0f)
+            ballVel.X -= temp * (p + 1.0f);
+        p = ballPos.X + scaledRad;
+        if (p > 1.0f)
+            ballVel.X -= temp * (p - 1.0f);
+        p = ballPos.Y - scaledRad;
+        if (p < -1.0f)
+            ballVel.Y -= temp * (p + 1.0f);
+        p = ballPos.Y + scaledRad;
+        if (p > 1.0f)
+            ballVel.Y -= temp * (p - 1.0f);
+        p = ballPos.Z - scaledRad;
+        if (p < -1.0f)
+            ballVel.Z -= temp * (p + 1.0f);
+        p = ballPos.Z + scaledRad;
+        if (p > 1.0f)
+            ballVel.Z -= temp * (p - 1.0f);
+           
+        //Push the balls away from each other.
+        for (int32 j = i + 1; j < N_BALLS; ++j)
+        {
+            float jRad = BallRadii[j] * BallPushbackRadiusScale;
+            float distSqr = (scaledRad + jRad);
+            distSqr *= distSqr;
+
+            if (FVector::DistSquared(ballPos, BallPoses[j]))
+            {
+                FVector forceForI = DeltaTime * BallPushback * (ballPos - BallPoses[j]);
+                ballVel += forceForI * invRad;
+                BallVelocities[j] -= forceForI / jRad;
+            }
+        }
+
+        //Apply velocity.
+        ballPos += ballVel * DeltaTime;
+
+        //If the ball is outside the edge of the bounds, snap back.
+        if (ballPos.X < -1.0f)
+        {
+            ballPos.X = -1.0f;
+            ballVel.X = FMath::Max(0.0f, ballVel.X);
+        }
+        if (ballPos.X > 1.0f)
+        {
+            ballPos.X = 1.0f;
+            ballVel.X = FMath::Min(0.0f, ballVel.X);
+        }
+        if (ballPos.Y < -1.0f)
+        {
+            ballPos.Y = -1.0f;
+            ballVel.Y = FMath::Max(0.0f, ballVel.Y);
+        }
+        if (ballPos.Y > 1.0f)
+        {
+            ballPos.Y = 1.0f;
+            ballVel.Y = FMath::Min(0.0f, ballVel.Y);
+        }
+        if (ballPos.Z < -1.0f)
+        {
+            ballPos.Z = -1.0f;
+            ballVel.Z = FMath::Max(0.0f, ballVel.Z);
+        }
+        if (ballPos.Z > 1.0f)
+        {
+            ballPos.Z = 1.0f;
+            ballVel.Z = FMath::Min(0.0f, ballVel.Z);
+        }
+
+#if UE_BUILD_DEBUG
+    GEngine>AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, "Test");
+#endif
+    }
+
+    localBallForce = FVector::ZeroVector;
+}
+
+void AShipMetaball::UpdateRenderingStuff(bool updateTex)
+{
+    MetaballMat = Cast<UMaterialInstanceDynamic>(MyRenderBox->GetMaterial(0));
+
+    MetaballMat->SetScalarParameterValue(TEXT("Emissive Brightness"), CurrentEmissive);
+    MetaballMat->SetScalarParameterValue(TEXT("Threshold"), Thinness);
+    MetaballMat->SetScalarParameterValue(TEXT("Increment"), Increment);
+    MetaballMat->SetVectorParameterValue(TEXT("Ball Color"), BallColor);
+    MetaballMat->SetVectorParameterValue(TEXT("Void Color"), VoidColor);
+    if(updateTex)
+    {
+        MetaballMat->SetTextureParameterValue(TEXT("Void Texture"), VoidTex);
+    }
+    MetaballMat->SetScalarParameterValue(TEXT("Void Texture Scale"), VoidTexScale);
+
+    for (int32 i = 0; i < N_BALLS; ++i)
+    {
+        FVector pos = GetTransform().TransformPosition(BallPoses[i]);
+        float radius = BallRadii[i] * RadiusScale;
+
+        FString str = TEXT("Ball ");
+        str.Append(FString::FromInt(i));
+
+        FString str2 = str;
+        str2.Append(" Pos");
+        MetaballMat->SetVectorParameterValue(FName(*str2), pos);
+
+        str2 = str;
+        str2.Append(" Radius");
+        MetaballMat->SetScalarParameterValue(FName(*str2), radius);
+    }
+}
---
+++ VRRacer/Source/VRRacer/Public/ShipMetaball.h
@@ -0,0 +1,85 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "ShipMetaball.generated.h"
+
+UCLASS()
+class VRRACER_API AShipMetaball : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AShipMetaball();
+
+    virtual void PostInitializeComponents() override;
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+
+    UFUNCTION(BlueprintCallable, Category = "Metaball Sim")
+    void AddBallForce(FVector worldForce);
+
+    UFUNCTION(BlueprintCallable, Category = "Metaball Sim")
+    void ResetVelocities();
+
+	
+    UStaticMeshComponent *MyRenderBox, *MyGlassContainer;
+
+    UMaterialInstanceDynamic *MetaballMat;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Metaball Sim")
+    TArray<float> BallRadii;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Metaball Sim")
+    TArray<FVector> BallPoses;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Metaball Sim")
+    TArray<FVector> BallVelocities;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Metaball Sim")
+    float BallPushback;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Metaball Sim")
+    float BallPushbackRadiusScale;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Metaball Rendering")
+    float MinEmissive;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Metaball Rendering")
+    float MaxEmissive;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Metaball Rendering")
+    float CurrentEmissive;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Metaball Rendering")
+    float Thinness;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Metaball Rendering")
+    float RadiusScale;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Metaball Rendering")
+    float Increment;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Metaball Rendering")
+    FColor VoidColor;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Metaball Rendering")
+    UTexture2D *VoidTex;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Metaball Rendering")
+    float VoidTexScale;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Metaball Rendering")
+    FColor BallColor;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Debug")
+    int32 UpdateVels;
+
+
+    //TODO: Music.
+
+private:
+
+    void UpdateRenderingStuff(bool updateTex);
+
+    FVector localBallForce;
+};
